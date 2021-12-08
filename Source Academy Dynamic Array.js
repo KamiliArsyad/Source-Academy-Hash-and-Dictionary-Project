55d3c4acea998fab65b6cb9f0ff01de3eba7ae10
Source Academy User <No public email provided>
2021-12-08 23:33:12 +0800

    Changes made from Source Academy

--- Source Academy Dynamic Array.js
+++ Source Academy Dynamic Array.js
@@ -1,3 +1,11 @@
+/*
+Alternative version of dynamic Array. Haven't been thoroughly analyzed yet.
+
+const buildDynamicArray = arr => f => f([arr]);
+const getDynArrLength = dynArr => array_length(dynArr(x => x)[0]);
+const displayDynArr = dynArr => display(dynArr(x => x));
+*/
+
 const buildDynamicArray = arr => [arr];
 const getDynArrLength = dynArr => array_length(dynArr[0]);
 const displayDynArr = dynArr => display(dynArr[0]);
@@ -24,6 +32,22 @@ function resizeTwice(dynArr){
     dynArr[0] = resizedArray;
 }
 
+//insert element into dynArr[index]
+function insert(element, index, dynArr) {
+    let temp = dynArr[0][index];
+    dynArr[0][index] = element;
+    
+    if(!is_undefined(temp)) {
+        for(let i = index + 1; !is_undefined(dynArr[0][i - 1]); i = i + 1) {
+            const garbage = dynArr[0][i];
+            dynArr[0][i] = temp;
+            temp = garbage;
+        }
+    }
+}
+
 const myArray = buildDynamicArray([1,2,3,4]);
-resizeTwice(myArray);
+
+insert('HELLOOO', 0, myArray);
+
 displayDynArr(myArray);
